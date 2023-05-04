76ee35aafa968d4965685b04ca2f861b4f2413f1
heiner <heiner.giehl@yahoo.com>
2023-05-05 01:49:01 +0200

    changed social bgColor

--- src/components/SocialMedia.tsx
+++ src/components/SocialMedia.tsx
@@ -6,18 +6,18 @@ import { AiFillYoutube } from "react-icons/ai/index"
 function SocialMedia() {
   return (
     <div className="shadow p-2 rounded flex flex-col bg-gray-200 glass-background glowing-shadow">
-      <div className="text-gray-500 hover:text-blue-500 transition duration-300 cursor-pointer">
+      <div className="text-white hover:text-blue-500 transition duration-300 cursor-pointer">
         <a href="https://github.com/thebeautyofcoding">
           {" "}
           <AiFillGithub size={38} />
         </a>
       </div>
-      <div className="text-gray-500 hover:text-blue-500 transition duration-300 cursor-pointer my-4">
+      <div className="text-white hover:text-blue-500 transition duration-300 cursor-pointer my-4">
         <a href="https://www.upwork.com/freelancers/~01e359856bc8297a0f">
           <SiUpwork size={38} />
         </a>
       </div>
-      <div className="text-gray-500 hover:text-blue-500 transition duration-300 cursor-pointer">
+      <div className="text-white hover:text-blue-500 transition duration-300 cursor-pointer">
         <a href="https://www.youtube.com/channel/UC7FZUBPLkay4dhoRw1W5ehw/videos">
           <AiFillYoutube size={38} />
         </a>
